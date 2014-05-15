7b7af9281e87aaddb80b24958202cf5720de164a
Julian Fischer <fischer@enterprise-rails.de>
2014-05-15 15:12:14 +0200

    Fixes tests

--- test/mailers/comment_notifications_test.rb
+++ test/mailers/comment_notifications_test.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class CommentNotificationsTest < ActionMailer::TestCase
   test "new_comment" do
-    mail = CommentNotifications.new_comment
+    comment = comments(:pauls_comment)
+    mail = CommentNotifications.new_comment(comment)
     assert_equal "New comment", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "A new comment has been submitted", mail.body.encoded
   end
 
 end
